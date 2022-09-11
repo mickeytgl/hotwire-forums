5ecb59a1783f831c45a8295c6367af617ddec3ec
Miguel <miguel.torres@hey.com>
2022-09-11 13:04:40 +0200

    Add discussion model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user, if: :user_signed_in?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def set_current_user
+    Current.user = current_user
   end
 end
